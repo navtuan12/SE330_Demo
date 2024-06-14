bf2ec20b9a02c3861cb5122e926493af2b19a163
anhtuan <anhtuan.mmt@gmail.com>
2024-06-14 07:45:09 +0700

    update: export jar

--- src/main/java/com/example/DHT22_App.java
+++ src/main/java/com/example/DHT22_App.java
@@ -13,8 +13,8 @@ public class DHT22_App {
     public static void main(String[] args) throws InterruptedException, IOException {
         var console = new Console();
         Context pi4j = Pi4J.newAutoContext();
-        int dataPinNum = 0xff;
-        String traceLevel = "info";
+        int dataPinNum = 7;
+        String traceLevel = "trace";
         console.title("<-- The Pi4J V2 Project Extension  -->", "DHT22_App");
         String helpString = " parms:  -d  data GPIO number  -t trace " +
             " \n -t  trace values : \"trace\", \"debug\", \"info\", \"warn\", \"error\" \n " +
@@ -33,32 +33,32 @@ public class DHT22_App {
             }
         });
 
-        for (int i = 0; i < args.length; i++) {
-            String o = args[i];
-            if (o.contentEquals("-d")) {
-                String a = args[i + 1];
-                dataPinNum = Integer.parseInt(a);
-                i++;
-            } else if (o.contentEquals("-t")) {
-                String a = args[i + 1];
-                i++;
-                traceLevel = a;
-                if (a.contentEquals("trace") | a.contentEquals("debug") | a.contentEquals("info") | a.contentEquals("warn") | a.contentEquals("error") | a.contentEquals("off")) {
-                    console.println("Changing trace level to : " + traceLevel);
-                } else {
-                    console.println("Changing trace level invalid  : " + traceLevel);
-                    System.exit(41);
-                }
-            } else if (o.contentEquals("-h")) {
-                console.println(helpString);
-                System.exit(41);
-            } else {
-                console.println("  !!! Invalid Parm " + o);
-                console.println(helpString);
-                System.exit(43);
-            }
-        }
-        
+        // for (int i = 0; i < args.length; i++) {
+        //     String o = args[i];
+        //     if (o.contentEquals("-d")) {
+        //         String a = args[i + 1];
+        //         dataPinNum = Integer.parseInt(a);
+        //         i++;
+        //     } else if (o.contentEquals("-t")) {
+        //         String a = args[i + 1];
+        //         i++;
+        //         traceLevel = a;
+        //         if (a.contentEquals("trace") | a.contentEquals("debug") | a.contentEquals("info") | a.contentEquals("warn") | a.contentEquals("error") | a.contentEquals("off")) {
+        //             console.println("Changing trace level to : " + traceLevel);
+        //         } else {
+        //             console.println("Changing trace level invalid  : " + traceLevel);
+        //             System.exit(41);
+        //         }
+        //     } else if (o.contentEquals("-h")) {
+        //         console.println(helpString);
+        //         System.exit(41);
+        //     } else {
+        //         console.println("  !!! Invalid Parm " + o);
+        //         console.println(helpString);
+        //         System.exit(43);
+        //     }
+        // }
+
         DHT22 sensor = new DHT22(pi4j, console, dataPinNum, traceLevel);
         sensor.readAndDisplayData();
 
